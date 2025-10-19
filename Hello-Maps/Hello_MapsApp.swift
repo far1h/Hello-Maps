ba6bcd77d8115aaaae9e9e292f9562a2d7284d9e
far1h <farihmuhammad45@gmail.com>
2025-10-19 23:15:56 +0700

    Initial Commit

---
+++ Hello-Maps/Hello_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_MapsApp.swift
+//  Hello-Maps
+//
+//  Created by Farih Muhammad on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hello_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
